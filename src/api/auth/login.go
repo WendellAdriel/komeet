87fe34e1b7be2b87ccb39900af441ad2ce958e9f
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-02-21 17:04:59 +0000

    Login DTO setup

--- src/api/auth/login.go
+++ src/api/auth/login.go
@@ -2,11 +2,30 @@ package auth
 
 import (
 	"github.com/gin-gonic/gin"
+	"komeet/utils"
 	"net/http"
 )
 
+type LoginDTO struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func login(c *gin.Context) {
+	var dto LoginDTO
+	if err := c.ShouldBind(&dto); err != nil {
+		utils.ErrorResponse(c, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	handleLogin(dto)
+
 	c.JSON(http.StatusNotImplemented, gin.H{
-		"message": "Not Implemented",
+		"email":    dto.Email,
+		"password": dto.Password,
 	})
+}
+
+func handleLogin(dto LoginDTO) {
+	// TODO:Implement
 }
---
+++ src/utils/responses.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"strings"
+	"time"
+)
+
+func ErrorResponse(c *gin.Context, code int, err error) {
+	c.JSON(code, gin.H{
+		"message": "Invalid data",
+		"errors":  parseError(err),
+	})
+}
+
+func parseError(errs ...error) []string {
+	var out []string
+	for _, err := range errs {
+		switch typedError := any(err).(type) {
+		case validator.ValidationErrors:
+			for _, e := range typedError {
+				out = append(out, parseFieldError(e))
+			}
+
+		case *json.UnmarshalTypeError:
+			out = append(out, parseMarshallingError(*typedError))
+
+		default:
+			out = append(out, err.Error())
+		}
+	}
+	return out
+}
+
+func parseFieldError(e validator.FieldError) string {
+	// workaround to the fact that the `gt|gtfield=Start` gets passed as an entire tag for some reason
+	// https://github.com/go-playground/validator/issues/926
+	fieldPrefix := fmt.Sprintf("The field %s", e.Field())
+	tag := strings.Split(e.Tag(), "|")[0]
+
+	switch tag {
+	case "required":
+		return fmt.Sprintf("%s is required", fieldPrefix)
+
+	case "required_without":
+		return fmt.Sprintf("%s is required if %s is not supplied", fieldPrefix, e.Param())
+
+	case "lt", "ltfield":
+		param := e.Param()
+		if param == "" {
+			param = time.Now().Format(time.RFC3339)
+		}
+		return fmt.Sprintf("%s must be less than %s", fieldPrefix, param)
+
+	case "gt", "gtfield":
+		param := e.Param()
+		if param == "" {
+			param = time.Now().Format(time.RFC3339)
+		}
+		return fmt.Sprintf("%s must be greater than %s", fieldPrefix, param)
+
+	default:
+		return fmt.Errorf("%v", e).Error()
+	}
+}
+
+func parseMarshallingError(e json.UnmarshalTypeError) string {
+	return fmt.Sprintf("The field %s must be a %s", e.Field, e.Type.String())
+}
