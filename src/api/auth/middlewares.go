1abecc6f1a9201c59977c8be306bd989aa3dfbfa
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-02-23 17:40:58 +0000

    Add method to get user from token

--- src/api/auth/middlewares.go
+++ src/api/auth/middlewares.go
@@ -1,32 +1,43 @@
 package auth
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	. "komeet/core"
+	"komeet/models"
 	"komeet/repositories"
 	"net/http"
 )
 
 func AuthRequired(c *gin.Context) {
+	_, err := GetUserFromToken(c)
+	if err != nil {
+		unauthorizedResponse(c)
+		return
+	}
+	c.Next()
+}
+
+func GetUserFromToken(c *gin.Context) (models.User, error) {
 	token := c.GetHeader(App.Config.Auth.TokenHeader)
 	tokenClaims, err := ParseTokenClaims(token)
 	if err != nil {
 		unauthorizedResponse(c)
-		return
+		return models.User{}, err
 	}
 
 	user, found := repositories.GetUserBy("uuid", tokenClaims.UserUUID)
 	if !found {
 		unauthorizedResponse(c)
-		return
+		return models.User{}, errors.New("unauthorized")
 	}
 
 	if user.AuthUUID != tokenClaims.AuthUUID {
 		unauthorizedResponse(c)
-		return
+		return models.User{}, errors.New("unauthorized")
 	}
 
-	c.Next()
+	return user, nil
 }
 
 func unauthorizedResponse(c *gin.Context) {
